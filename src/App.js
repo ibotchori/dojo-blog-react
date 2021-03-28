ea5db58bcc260a78d6f7c7eab746273d9acf31b0
koregeo <koregm@gmail.com>
2021-03-29 01:18:37 +0400

    Multiple Components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import './App.css';
+import Navbar from './Navbar'
+import Home from './Home'
 
 function App() {
-  const title = 'Welcome to the new blog'  // <-- create variable
-  const likes = 50
-  /* const person = { name: "John", age: 30 } */
-  const link = "http://www.google.com"
-
   return (
     <div className="App">
-      <div className="content"> {/* use the variable */}
-        <h1>{title}</h1>
-        <p>Liked {likes} times</p>
-        {/*  <p>{persom}</p> */}
-        <p>{10}</p>
-        <p>{"Hello World"}</p>
-        <p>{[1, 2, 3, 4, 5, 6]}</p>
-        <p>{Math.random() * 10}</p>
-
-        <a href={link}>Google Site</a>
+      <Navbar />
+      <div className="content">
+        <Home />
       </div>
     </div>
   );
---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return (
+        <nav>
+            <h1>The Dojo Blog</h1>
+            <div className="linsk">
+                <a href="/">Home</a>
+                <a href="/create">New Blog</a>
+            </div>
+
+        </nav>
+    )
+}
+
+export default Navbar
