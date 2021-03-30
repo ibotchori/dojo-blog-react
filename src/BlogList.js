d11aa0d1e7a7a3725f69a442c2484de7274e7cf9
koregeo <koregm@gmail.com>
2021-03-30 22:55:49 +0400

    Props

---
+++ src/BlogList.js
@@ -0,0 +1,18 @@
+const BlogList = (props) => { // <-- receive props
+    const blogs = props.blogs // <-- receive blogs from Home component
+    const title = props.title
+
+    return (
+        <div className="blog-list">
+            <h2>{title}</h2>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default BlogList;
