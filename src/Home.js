c98d99a24216ad163885c74f86ad186fe9c9e60c
koregeo <koregm@gmail.com>
2021-03-31 18:59:59 +0400

    Conditional Loading Message

--- src/Home.js
+++ src/Home.js
@@ -3,15 +3,19 @@ import BlogList from './BlogList'
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null)
+    const [isPendign, setIsPending] = useState(true)
 
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-            .then(res => {
-                return res.json()
-            })
-            .then(data => {
-                setBlogs(data) // <-- updates blogs state
-            })
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
+                .then(res => {
+                    return res.json()
+                })
+                .then(data => {
+                    setBlogs(data) // <-- updates blogs state
+                    setIsPending(false) // <-- after data is come, state becomes false
+                })
+        }, 1000);
     }, []) // <-- runs only first render 
 
     /* // same process with async
@@ -24,6 +28,7 @@ const Home = () => {
 
     return (
         <div className="home">
+            {isPendign && <div>Loading...</div>} {/* <-- show this div only when isPending state is true */}
             {/* renders Bloglist after only blogs state will true, from the beginning blogs is null because we fill it from fech*/}
             {blogs && <BlogList blogs={blogs} title="All BLogs" />}
         </div>
@@ -32,3 +37,6 @@ const Home = () => {
 }
 
 export default Home
+
+
+// command for Json Server: npx json-server --watch data/db.json --port 8000
