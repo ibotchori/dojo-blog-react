8adc282eeaeade60c560b9b587d7b96ce75262f1
koregeo <koregm@gmail.com>
2021-04-01 21:12:56 +0400

    useEffect Cleanup (AbortController)

--- src/useFetch.js
+++ src/useFetch.js
@@ -6,8 +6,10 @@ const useFetch = (url) => { // the url will received from component
     const [error, setError] = useState(null)
 
     useEffect(() => {
+        const abortCont = new AbortController() // <-- create abort controller
+
         setTimeout(() => {
-            fetch(url)
+            fetch(url, { signal: abortCont.signal }) // <-- add second parameter to associate AbortController to fetch
                 .then(res => {
 
                     if (!res.ok) { // <-- if server is unrichable. res.ok = falce
@@ -21,10 +23,17 @@ const useFetch = (url) => { // the url will received from component
                     setError(null) // <-- get rid of the error message if fetch is successful
                 })
                 .catch(err => {
-                    setIsPending(false) // <-- if we get the error laodnig message does not shown
-                    setError(err.message)  // <-- update state with error throw by catch block
+                    if (err.name === 'AbortError') { // <-- dont update state if fetch aborted
+                        console.log('fetch aborted')
+                    } else {
+                        setIsPending(false) // <-- if we get the error laodnig message does not shown
+                        setError(err.message)  // <-- update state with error throw by catch block
+                    }
+
                 })
         }, 1000);
+
+        return () => abortCont.abort() // <-- abort fetch if page reload
     }, [url]) // <-- re-run this function when the url changes
 
     return { data, isPendign, error } // <-- send properties to component which will uses this hook
